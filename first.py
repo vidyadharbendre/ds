7a1b5281c6c8658a95e790eeac1763b08aec04e1
Vidyadhar Bendre <vidyadhar.bendre@gmail.com>
2023-06-12 20:18:18 +0530

    added pizza

--- first.py
+++ first.py
@@ -1,1 +1,6 @@
-print('Hey, This is my first program at pycharm editor')
+hungry = input('Are you hungry')
+if hungry == 'Y':
+    print('eat samosa')
+    print('eat pizza')
+else:
+    print('do your homework')
